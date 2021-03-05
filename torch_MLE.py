ee60d098bb4d0d481e236e3189a994ddefb12ca9
ChristianDjurhuus <57835777+ChristianDjurhuus@users.noreply.github.com>
2021-03-05 15:51:20 +0100

    Added script for the maximum likelihood optimization

---
+++ torch_MLE.py
@@ -0,0 +1,47 @@
+import torch
+import os
+from scipy.io import mmread
+
+
+#Creating dataset
+
+os.chdir('/Users/christiandjurhuus/PycharmProjects/Learning-and-Visualizing-Bipartite-Network-Embeddings/Datasets/divorce')
+
+text_file = 'divorce.mtx'
+
+
+#Loading data and making adjancency matrix
+raw_data = mmread(text_file)
+#print(raw_data)
+
+A = raw_data.todense()
+print(A.shape)
+
+class LSM():
+    def __init__(self, A, input_size, latent_dim):
+        self.A = A
+        self.input_size = input_size
+        self.latent_dim = latent_dim
+
+        self.beta = torch.nn.Parameter(torch.randn(self.input_size,device=device))
+        self.gamma = torch.nn.Parameter(torch.randn(self.input_size,device=device))
+
+        self.latent_zi = torch.nn.Parameter(torch.randn(self.input_size, self.latent_dim, device=device))
+        self.latent_zj = torch.nn.Parameter(torch.randn(self.input_size, self.latent_dim, device=device))
+
+
+        self.z_dist = torch.pairwise_distance(self.latent_zi, self.latent_zj, p=2)
+
+
+    def log_likelihood(self):
+        Lambda = self.beta + self.gamma - abs(self.z_dist)
+
+        LL += (torch.sum(torch.sparse.mm(A, Lambda)) - torch.sum(torch.exp(Lambda)))
+
+        LL += torch.sum(torch.sum(torch.sparse.mm(A, Lambda) - torch.exp(Lambda), axis = 1), axis = 0)
+
+
+
+
+
+

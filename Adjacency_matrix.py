fa286d2ab27ff3bf04ad64a896e842b3547007e2
Laitallis <57214933+Laitallis@users.noreply.github.com>
2021-03-10 10:09:42 +0100

    Distance matrix

--- Adjacency_matrix.py
+++ Adjacency_matrix.py
@@ -1,12 +1,20 @@
 import torch
-# Transform the Biadjacency matrix to a square adjacency for bipartite graphs.
-def From_Biadjacency_To_Adjacency(B):
-    r, c = list(B.size())
-    Adjacency_Matrix = torch.cat((torch.cat((torch.zeros(r,r), B),1),
-                                  torch.cat((torch.transpose(B,0,1), torch.zeros(c,c)),1)),0)
-    return Adjacency_Matrix
+
+
+class Preprocessing():
+    def __init__(self, B):
+        self.B = B
+
+    def From_Biadjacency_To_Adjacency(self, B):
+        """ Transform the Biadjacency matrix to a square adjacency for bipartite graphs.
+        """
+        r, c = list(B.size())
+        Adjacency_Matrix = torch.cat((torch.cat((torch.zeros(r,r), B),1),
+                                      torch.cat((torch.transpose(B,0,1), torch.zeros(c,c)),1)),0)
+        return Adjacency_Matrix
 
 if __name__ == "__main__":
     B = torch.rand(50,9)
-    print(From_Biadjacency_To_Adjacency(B))
-    print(From_Biadjacency_To_Adjacency(B).size())
+    preproc = Preprocessing(B)
+    print(preproc.From_Biadjacency_To_Adjacency(B))
+    print(preproc.From_Biadjacency_To_Adjacency(B).size())

13f28b494c103a9683a45f85d3da1e0d347119b5
ChristianDjurhuus <57835777+ChristianDjurhuus@users.noreply.github.com>
2021-03-10 15:31:28 +0100

    Fixed pytorch version - Nikoloas

--- torch_MLE.py
+++ torch_MLE.py
@@ -7,7 +7,7 @@ from Adjacency_matrix import Preprocessing
 
 #Creating dataset
 
-os.chdir('C:/Users/Daniel/Documents/DTU/Reinforcement/Learning-and-Visualizing-Bipartite-Network-Embeddings/Datasets/divorce')
+os.chdir('/Users/christiandjurhuus/PycharmProjects/Learning-and-Visualizing-Bipartite-Network-Embeddings/Datasets/divorce')
 
 text_file = 'divorce.mtx'
 
@@ -20,50 +20,60 @@ A = raw_data.todense()
 A = torch.tensor(A)
 #print(A.shape)
 
-class LSM():
+class LSM(nn.Module):
     def __init__(self, B, input_size, latent_dim):
+        super(LSM, self).__init__()
         self.A = B
         self.input_size = input_size
         self.latent_dim = latent_dim
 
-        self.beta = torch.nn.Parameter(torch.randn(self.input_size))
-        self.gamma = torch.nn.Parameter(torch.randn(self.input_size))
+        self.beta = torch.nn.Parameter(torch.randn(self.input_size[0]))
+        self.gamma = torch.nn.Parameter(torch.randn(self.input_size[1]))
 
-        self.latent_zi = torch.nn.Parameter(torch.randn(self.input_size, self.latent_dim))
-        self.latent_zj = torch.nn.Parameter(torch.randn(self.input_size, self.latent_dim))
+        self.latent_zi = torch.nn.Parameter(torch.randn(self.input_size[0], self.latent_dim))
+        self.latent_zj = torch.nn.Parameter(torch.randn(self.input_size[1], self.latent_dim))
 
-        self.myparameters = nn.ParameterList([self.latent_zi, self.latent_zj, self.beta, self.gamma])
+#        self.myparameters = nn.ParameterList([self.latent_zi, self.latent_zj, self.beta, self.gamma])
         #self.nn_layers = nn.Modulelist([self.latent_zi, self.latent_zj])
 
 
     def log_likelihood(self, A):
 
         #self.p_dist = torch.pairwise_distance(self.latent_zi, self.latent_zj, p=2)
-        z_dist = (((torch.unsqueeze(self.latent_zi, 1) - self.latent_zj)**2).sum(-1))**0.5
+        z_dist = (((torch.unsqueeze(self.latent_zi, 1) - self.latent_zj+1e-06)**2).sum(-1))**0.5
+        bias_matrix = torch.unsqueeze(self.beta, 1) + self.gamma
 
-        Lambda = torch.unsqueeze(self.beta, 1) + self.gamma - abs(z_dist)
+        Lambda = bias_matrix - z_dist
+        LL = (A*Lambda).sum() - torch.sum(torch.exp(Lambda))
 
-        LL += torch.unsqueeze(torch.sparse.mm(A, Lambda),1).sum() - torch.sum(torch.exp(Lambda))
         #LL = (torch.sum(torch.sparse.mm(A, Lambda)) - torch.sum(torch.exp(Lambda)))
         #LL = torch.sum(torch.sum(torch.sparse.mm(A, Lambda) - torch.exp(Lambda), dim=0), dim=1)
         #LL = torch.sum(torch.sparse.mm(A, Lambda) - torch.exp(Lambda), dim=0) * torch.sum(torch.sparse.mm(A, Lambda) - torch.exp(Lambda), dim=1)
-
+        return LL
+''' 
     def optimizer(self, iterations):
         # Implements stochastic gradient descent (optionally with momentum). Nesterov momentum
-
         for _ in range(iterations):
-            model = nn.LogSoftmax()
-            optimizer = optim.SGD(params=model.parameters(), lr=0.01, momentum=0.9)
-            loss = -model.log_likelihood(self.A)/ self.input_size
+            optimizer = optim.SGD(params=self.parameters(), lr=0.01, momentum=0.9)
+            loss = -self.log_likelihood(self.A)/ self.input_size
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
         return loss
+'''
 
 if __name__ == "__main__":
     preproc = Preprocessing()
-    model = LSM(B=preproc.From_Biadjacency_To_Adjacency(A), input_size=A.shape[0], latent_dim=2)
-    B = model.optimizer(10)
-
+#    model = LSM(B=preproc.From_Biadjacency_To_Adjacency(A), input_size=A.shape[0], latent_dim=2)
+    model = LSM(B=A, input_size=A.shape, latent_dim=2)
+#    B = model.optimizer(10)
+    optimizer = optim.SGD(params=model.parameters(), lr=0.01, momentum=0.9)
+    for _ in range(10):
+        loss = -model.log_likelihood(model.A) / model.input_size[0]
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(loss.item())
 
 
 

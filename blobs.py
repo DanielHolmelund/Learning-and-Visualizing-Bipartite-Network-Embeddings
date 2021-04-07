fcf11c57c93d7c0d4746c52d33c8a45cd96fdfcd
Ne0-1 <57833596+Ne0-1@users.noreply.github.com>
2021-04-07 21:41:29 +0200

    Adding blobs

--- blobs.py
+++ blobs.py
@@ -3,9 +3,10 @@
 Created on Wed Oct  7 10:16:19 2020
 
 @author: nnak
+
 """
 
-from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial.distance import pdist
@@ -15,22 +16,22 @@ scaler = StandardScaler()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #device = torch.device("cpu")
 
-torch.set_default_tensor_type('torch.cuda.FloatTensor')
+#torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 
 
-
+# Create embeddings
 X, y = make_blobs(n_samples=np.repeat(100,10), n_features=2)
 
-lab=y
 
-plt.scatter(X[:, 0], X[:, 1], c=y, s= 30000 / len(X), cmap="tab10")
+# Create plot
+'''plt.scatter(X[:, 0], X[:, 1], c=y, s= 30000 / len(X), cmap="tab10")
     #plt.axis([0,1,0,1]) ; plt.tight_layout()
 plt.title('True latent variables')
 plt.xlabel('z1')
 plt.ylabel('z2')
 
-plt.show()
+plt.show()'''
 
 H=pdist(X, metric='euclidean')
 
@@ -50,6 +51,8 @@ def generate_network_bias(H,X,graph_type='undirected'):
     #Generate zero count probability
     inv_logit=torch.exp(-torch.exp(logit_u))
     #sample from Uniform [0,1]
+    print(inv_logit)
+    print(inv_logit.shape)
     adj_u=torch.rand(inv_logit.shape)
     #
     adj_u[adj_u<inv_logit]=0
@@ -66,12 +69,12 @@ def generate_network_bias(H,X,graph_type='undirected'):
 
 
 adj_m=generate_network_bias(torch.from_numpy(H).float().to(device),torch.from_numpy(X).float().to(device))
-    
+
 adj_m=adj_m.cpu().data.numpy()
 full_rank=adj_m+adj_m.transpose()
 edges=(full_rank==1).sum()
 X=torch.from_numpy(X).float().to(device)
-
-
+print(adj_m)
+asa=1
 
 
